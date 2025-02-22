0137ebd6f66d7d794dcb44f6a3ad588fb19e8990
CuppaKe <turnman87@gmail.com>
2025-02-22 15:19:58 +0400

    feat: connect products service

--- aws/lib/aws-stack.ts
+++ aws/lib/aws-stack.ts
@@ -3,7 +3,7 @@ import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
-import * as cloudfrontorigins from "aws-cdk-lib/aws-cloudfront-origins"; // Add this import
+import * as cloudfrontorigins from "aws-cdk-lib/aws-cloudfront-origins";
 import * as iam from "aws-cdk-lib/aws-iam";
 
 export class ReactAppDeploymentStack extends cdk.Stack {
--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://sv3p7oerwe.execute-api.us-east-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://sv3p7oerwe.execute-api.us-east-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
