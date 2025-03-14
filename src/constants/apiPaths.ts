8a79bb19e378d990fc018fa0c0a32722e8634fb9
CuppaKe <43150373+CuppaKe@users.noreply.github.com>
2025-03-14 12:24:46 +0400

    feat: add upload link and add invalidation (#3)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://sv3p7oerwe.execute-api.us-east-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://r0sj9ztdl7.execute-api.us-east-1.amazonaws.com/",
   bff: "https://sv3p7oerwe.execute-api.us-east-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
