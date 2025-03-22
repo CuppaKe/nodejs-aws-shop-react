325b03f973ac5399112e54422caa897e44756238
CuppaKe <turnman87@gmail.com>
2025-03-22 13:50:11 +0400

    feat: add auth header to import file feature

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
-  product: "https://sv3p7oerwe.execute-api.us-east-1.amazonaws.com",
+  product: "https://0xe9x2so14.execute-api.us-east-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://r0sj9ztdl7.execute-api.us-east-1.amazonaws.com/",
-  bff: "https://sv3p7oerwe.execute-api.us-east-1.amazonaws.com",
+  import: "https://9zmmbyecgi.execute-api.us-east-1.amazonaws.com/prod",
+  bff: "https://0xe9x2so14.execute-api.us-east-1.amazonaws.com/prod",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
