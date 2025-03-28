aa831f382a39ab63eabfe9160721a9bc06de6702
CuppaKe <43150373+CuppaKe@users.noreply.github.com>
2025-03-28 12:18:37 +0400

    feat: add auth header to import file feature (#4)

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -4,65 +4,71 @@ import Box from "@mui/material/Box";
 import axios from "axios";
 
 type CSVFileImportProps = {
-  url: string;
-  title: string;
+    url: string;
+    title: string;
 };
 
 export default function CSVFileImport({ url, title }: CSVFileImportProps) {
-  const [file, setFile] = React.useState<File>();
+    const [file, setFile] = React.useState<File>();
 
-  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const files = e.target.files;
-    if (files && files.length > 0) {
-      const file = files[0];
-      setFile(file);
-    }
-  };
-
-  const removeFile = () => {
-    setFile(undefined);
-  };
-
-  const uploadFile = async () => {
-    console.log("uploadFile to", url);
-
-    try {
-        if(!file) {
-            throw new Error("No file selected");
+    const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const files = e.target.files;
+        if (files && files.length > 0) {
+            const file = files[0];
+            setFile(file);
         }
+    };
 
-        const response = await axios({
-            method: "GET",
-            url,
-            params: {
-              name: encodeURIComponent(file.name),
-            },
-          });
-          console.log("File to upload: ", file.name);
-          console.log("Uploading to: ", response.data);
-          const result = await fetch(response.data, {
-            method: "PUT",
-            body: file,
-          });
-          console.log("Result: ", result);
-          setFile(undefined);
-    } catch (error) {
-        console.log("Error: ", error);
-    }
-  };
-  return (
-    <Box>
-      <Typography variant="h6" gutterBottom>
-        {title}
-      </Typography>
-      {!file ? (
-        <input type="file" onChange={onFileChange} />
-      ) : (
-        <div>
-          <button onClick={removeFile}>Remove file</button>
-          <button onClick={uploadFile}>Upload file</button>
-        </div>
-      )}
-    </Box>
-  );
+    const removeFile = () => {
+        setFile(undefined);
+    };
+
+    const uploadFile = async () => {
+        console.log("uploadFile to", url);
+
+        try {
+            if (!file) {
+                throw new Error("No file selected");
+            }
+
+            const cred = localStorage.getItem("authorization_token");
+            const auth = cred ? `Basic ${cred}` : "";
+
+            const response = await axios({
+                method: "GET",
+                url,
+                params: {
+                    name: encodeURIComponent(file.name),
+                },
+                headers: {
+                    Authorization: auth,
+                },
+            });
+            console.log("File to upload: ", file.name);
+            console.log("Uploading to: ", response.data);
+            const result = await fetch(response.data, {
+                method: "PUT",
+                body: file,
+            });
+            console.log("Result: ", result);
+            setFile(undefined);
+        } catch (error) {
+            console.log("Error: ", error);
+        }
+    };
+    return (
+        <Box>
+            <Typography variant="h6" gutterBottom>
+                {title}
+            </Typography>
+            {!file ? (
+                <input type="file" onChange={onFileChange} />
+            ) : (
+                <div>
+                    <button onClick={removeFile}>Remove file</button>
+                    <button onClick={uploadFile}>Upload file</button>
+                </div>
+            )}
+        </Box>
+    );
 }
